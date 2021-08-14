faaff3b02b5720833062d080fb4ebdf7b4354d03
Wi-ll-iam <Will_Simpson12@yahoo.com>
2021-08-14 14:20:34 -0500

    adding mongodb

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require(`express`);
 const Joi = require(`joi`);
 const helmet = require(`helmet`);
 const morgan = require(`morgan`);
+const mongoose = require(`mongoose`);
 const app = express();
 const startupDebugger = require(`debug`)(`app:startup`);
 const genres = require(`./routes/genres`);
+const { boolean } = require("joi");
 
 require(`dotenv`).config();
 
@@ -21,6 +23,28 @@ if(process.env.NODE_ENV === `development`){
 app.use(morgan(`tiny`));
 app.use(`/api/genres`, genres);
 
+//DB Connect
+mongoose.connect(process.env.MONGOOSE_CONNECTION)
+    .then(() => console.log(`Connected to MongoDB...`))
+    .catch((err) => console.error(`Could not connect to MongoDB...`, err));
+
+const movieSchema = new mongoose.Schema({
+    name: String,
+    genre: String,
+    director: String,
+    tags: [ String ],
+    isPublished: Boolean
+});
+
+const Movie = mongoose.model(`Movie`, movieSchema);
+
+const movie = new Movie({
+    name: `Dune`,
+    genre: `Scifi`,
+    director: `Denis Villeneuve`,
+    tags: [`sand`, `worm`, `spice`, `Timothee Chalamet`, `Zendaya`, `Oscar Isaac`, `Jason Momoa`, `Dave Bautista`, `Stellan Skarsgard`],
+    isPublished: true
+});
 
 //Listen
 const appPort = process.env.PORT || 3000;
